5d93895cea9d74efd2da5484ea5ff781d91cb644
Yauhen Khasenevich <khasenevich@Yauhens-MacBook-Pro.local>
2016-04-05 21:49:11 +0300

    Usage and login command added

--- slacker-image/build.sh
+++ slacker-image/build.sh
@@ -10,24 +10,45 @@ build() {
 	echo "Building docker image..."
 
 	docker build -t $docker_image .
-
-	echo "Building docker image..."
-
 }
 
 push() {
-	echo "push $tag"
+	docker push $docker_image
 }
 
+login() {
+	docker login
+}
 
+usage() {
+	echo
+	echo "Usage:"
+	echo "	./build.sh [login] [build] [push] [tag DOCKER_IMAGE_TAG]"
+	echo
+	echo "	Default tag is 'latest'."
+	echo "	You can use several commands at once."
+	echo
+	echo "Example:"
+	echo "	./build.sh build"
+	echo "		this will build image $docker_image"
+	echo
+	echo "	./build.sh build tag v5"
+	echo "		this will build image $docker_image_name:v5"
+	echo
+	echo "	./build.sh login push"
+	echo "		this will log you in to dockerhub and push image $docker_image"
+	echo
+	echo
+}
 
+# parsing arguments
 
 args=( "$@" )
 cmds=()
 
 for (( i=0; i<=${#args[@]}; i++ ))
 do
-   	if [ "${args[$i]}" = 'build' ] || [ "${args[$i]}" = 'push' ]
+  	if [ "${args[$i]}" = 'build' ] || [ "${args[$i]}" = 'push' ] || [ "${args[$i]}" = 'login' ]
    		then
    			cmds+=("${args[$i]}")
    	fi
@@ -40,6 +61,12 @@ do
 done
 
 docker_image="$docker_image_name:$docker_image_tag"
+
+if [ ${#cmds[@]} = 0 ]
+	then
+		usage
+		exit 0
+fi
 
 for cmd in ${cmds[@]}
 do
